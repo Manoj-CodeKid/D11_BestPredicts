bba306b9e2913510ae75577a049f9c32e7cd3cbe
Manoj-CodeKid <manojg.er@gmail.com>
2023-02-01 21:57:29 +0530

    With user inputs for winner predictions, match_time

--- D11_Predictor_MainFile.py
+++ D11_Predictor_MainFile.py
@@ -1,11 +1,19 @@
 import pandas as pd
-read_cols=range(0,6)
+read_cols="A:F"
 Dataframe1=pd.read_excel('Cricketit.xlsm',usecols=read_cols,nrows=11)
 print(Dataframe1)
+Team1_name=Dataframe1.columns.values[0]
+Team2_name=Dataframe1.columns.values[3]
 pitch_input=int(input("Press 1 for Batting, Press 2 for Balanced, Press 3 for Bowling : "))
 if pitch_input==1:
-    print("User input is Batting Pitch")
+    print("Your input is Batting Pitch")
 elif pitch_input==2:
-    print("User input is Balanced Pitch")
+    print("Your input is Balanced Pitch")
 else:
-    print("User input is Bowling Pitch")
+    print("Your input is Bowling Pitch")
+winner_predictor=int(input("Whom do you predict as winner in this match. Press 1 for {}, 2 for {}: ".format(Team1_name,Team2_name)))
+if winner_predictor==1:
+    print("Your Winner prediction for this match is {}".format(Team1_name))
+else:
+    print("Your Winner prediction for this match is {}".format(Team2_name))
+match_time=int(input("Press 1 for day game and 2 for day night game : "))
